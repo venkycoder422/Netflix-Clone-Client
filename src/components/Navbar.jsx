2630d49818c7f0e0337c7e3edb1624a7ff123028
venky422 <dommaravenkatesulu22@gmail.com>
2022-12-11 00:26:28 +0530

    created Navbar and search with mic feaure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,202 @@
 import React, { useState } from 'react'
 import styled from 'styled-components';
 import logo from '../assets/Netflix-Logo.png';
-import { Link } from 'react-router-dom';
-import { FaSearch } from 'react-icons/fa'
+import { Link, useNavigate } from 'react-router-dom';
+import { FaMicrophone, FaPowerOff, FaSearch } from 'react-icons/fa'
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { firebaseAuth } from '../utils/firebase-config';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from 'react-speech-recognition';
 export const Navbar = ({ scroll }) => {
+  const navigate = useNavigate();
   const links = [
     { name: "Home", link: '/' },
     { name: "TV Shows", link: '/tv' },
     { name: "Movies", link: '/movies' },
-    { name: "My List", link: '/mylist' }
+    { name: "New & Popular", link: '/latest' },
+    { name: "My List", link: '/mylist' },
+    { name: "Browse by Langauges", link: '/langauages' }
   ];
   const [showSearch, setSearch] = useState(false);
   const [hover, setHover] = useState(false);
+  const [onmic,setOnMic] = useState(false);
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+console.log(transcript);
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (!currentUser) navigate('/login');
+  })
+
   return (
     <Container>
-      <nav className={`flex ${scroll ? "scroll" : ""}`}>
+      <nav className={`flex ${scroll ? "scroll" : ""}`} >
         <div className="left flex a-center">
           <div className="brand flex a-center j-center">
             <img src={logo} alt="logo" />
-            <ul>
-              {
-                links.map((name, link) => {
-                  return (
-                    <li key={name}>
-                      <Link to={link}>{name}</Link>
-                    </li>
-                  );
-                })
-              }
-            </ul>
+          </div>
+          <ul className="links flex" >
+            {
+              links.map(({ name, link }) => {
+                return (
+                  <li key={name}>
+                    <Link to={link}>{name}</Link>
+                  </li>
+                );
+              })
+            }
+          </ul>
 
-          </div>
-          <div className="right flex a-center">
-            <div className={`search ${showSearch ? 'search' : ""}`}>
-              <button onFocus={() => setSearch(true)} onBlur={() => { if (!hover) setSearch(false) }}>
-                <FaSearch />
-              </button>
-              <input type="text" placeholder='Search' onMouseEnter={(e) => setHover(true)}
-                onMouseLeave={(e) => setHover(false)}
-                onBlur={()=>showSearch(false)} />          </div>
-          </div>
         </div>
+        <div className="right flex a-center">
+          <button className='microPhone'
+          onClick={SpeechRecognition.startListening}
+          
+           onFocus={() =>{setSearch(true)}}
+           onBlur={() => SpeechRecognition.stopListening}
+          >
+            <FaMicrophone />
+          </button>
+          <div className={`search ${showSearch ? 'show-search' : ''}`}>
+
+            <button
+              onFocus={() => setSearch(true)}
+              onBlur={() => { if (!hover) setSearch(false) }}
+            >
+              <FaSearch />
+            </button>
+
+            <input type="text" placeholder='Titles, People, Generes' autoComplete='on'
+              
+              value={onmic?`${transcript}`:""}
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}
+              onBlur={() => {
+                setSearch(false);
+                setHover(false);
+              }}
+            />
+          </div>
+          <button onClick={() => signOut(firebaseAuth)}>
+            <FaPowerOff />
+          </button>
+        </div>
+
       </nav>
-    </Container>
+    </Container >
   )
 }
 
 const Container = styled.div`
+.scroll{
+  background-color:black;
+}
 
+nav{
+  position:sticky;
+  top:0;
+  height:6.5rem;
+  width:100%;
+  justify-content:space-between;
+  position:fixed;
+  padding:0 4rem;
+  z-index:2;
+  align-items:center;
+  transition:0.3s ease-in-out;
+
+}
+
+.left{
+  gap:2rem;
+  .brand{
+    img{
+      height:4rem;
+    }
+  }
+  .links{
+    list-style-type:none;
+    gap:2rem;
+    li{
+      a{
+        color:white;
+        text-decoration:none;
+      }
+    }
+  }
+}
+
+.right{
+  gap:1rem;
+
+  button{
+    background-color:tranparent;
+    border:none;
+    cursor:pointer;
+    &:focus {
+      outline:none;
+    }
+    svg{
+      color:#f34242;
+      font-size:1.2rem;
+    }
+  }
+
+  .microPhone{
+    background-color:transparent;
+    svg{
+      color:white;
+    }
+  }
+
+  .search{
+    display:flex;
+    gap:0.4rem;
+    align-items:center;
+    justify-content:center;
+    padding:0.2rem;
+    padding-left:0.5rem;
+    button{
+      background-color:transparent;
+      svg{
+        color:white;
+      }
+    }
+
+    input{
+      width:0;
+      opacity:0;
+      visibility:hidden;
+      transition:0.3s ease-in-out;
+      background-color:transparent;
+      color:white;
+      border:none;
+      &:focus{
+        outline:none;
+      }
+      
+    }
+  }
+  
+  .show-search{
+    border: 1px solid white;
+    background-color:rgba(0,0,0,0.6);
+    .Micro{
+      svg{
+        color:white;
+      }
+    }
+    input{
+      width:100%;
+      opacity:1;
+      visibility:visible;
+      padding:0.3rem;
+      transition:0.3s ease-in-out;
+    }
+  }
+  
+}
 `
